751a93a46698b7bc54e313f2a77bb8fa2fc83c48
ian <me@iany.me>
2017-03-29 18:32:42 +0800

    MessageBox

---
+++ Assets/tinyfiledialogs/Scripts/tinyfd/MessageBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace tinyfd {
+	public class MessageBox : MonoBehaviour {
+		public string title;
+		public string message;
+		public TinyFileDialogs.DialogType dialogType = TinyFileDialogs.DialogType.ok;
+		public TinyFileDialogs.IconType iconType = TinyFileDialogs.IconType.info;
+		public bool okIsDefault = true;
+
+		public bool Show() {
+			return TinyFileDialogs.MessageBox(title, message, dialogType, iconType, okIsDefault);
+		}
+
+		public void ShowCallback() {
+			Show();
+		}
+	}
+}
---
+++ Assets/tinyfiledialogs/Scripts/tinyfd/TinyFileDialogs.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace tinyfd {
+
+public static class TinyFileDialogs {
+#if UNITY_IPHONE && !UNITY_EDITOR
+	private const string DLL_NAME = "__Internal";
+#else
+	private const string DLL_NAME = "tinyfiledialogs";
+#endif
+
+	public enum DialogType {
+		ok,
+		okcancel,
+		yesno
+	}
+	public enum IconType {
+		info,
+		warning,
+		error,
+		question
+	}
+
+	// int tinyfd_messageBox (
+	// char const * const aTitle , /* "" */
+	// char const * const aMessage , /* "" may contain \n \t */
+	// char const * const aDialogType , /* "ok" "okcancel" "yesno" */
+	// char const * const aIconType , /* "info" "warning" "error" "question" */
+	// int const aDefaultButton ) ; /* 0 for cancel/no , 1 for ok/yes */
+	// /* returns 0 for cancel/no , 1 for ok/yes */
+
+	[DllImport(DLL_NAME)]
+	private static extern int tinyfd_messageBox(string title, string message, string dialogType, string iconType, int defaultButton);
+
+	public static bool MessageBox(
+		string title,
+		string message,
+		DialogType dialogType = DialogType.ok,
+		IconType iconType = IconType.info,
+		bool okIsDefault = true
+	) {
+		return 0 != tinyfd_messageBox(title, message, dialogType.ToString(), iconType.ToString(), okIsDefault ? 1 : 0);
+	}
+}
+
+}
