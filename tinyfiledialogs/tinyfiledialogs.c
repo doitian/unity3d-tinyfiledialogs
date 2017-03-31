f7b71ea331fcd2d7f0c5403efb970a20bdf2a177
zhangxudong1 <zhangxudong@3pjgames.com>
2017-03-31 09:18:44 +0800

    Fix windows encoding

--- tinyfiledialogs/tinyfiledialogs.c
+++ tinyfiledialogs/tinyfiledialogs.c
@@ -103,7 +103,7 @@ misrepresented as being the original software.
  #include <conio.h>
  /*#include <io.h>*/
  #define SLASH "\\"
- int tinyfd_winUtf8 = 0 ; /* on windows string char can be 0:MBSC or 1:UTF-8 */
+ int tinyfd_winUtf8 = 1 ; /* on windows string char can be 0:MBSC or 1:UTF-8 */
 #else
  #include <limits.h>
  #include <unistd.h>
